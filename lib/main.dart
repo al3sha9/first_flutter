b442179614fa43a70f687f35be715b542583df4b
alishan <optimist.alishan@gmail.com>
2023-08-01 00:23:52 +0500

    initial

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/myhome.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Color(0xff333333),
+      title: 'NIGGER',
+      home: Homepage(),
+
+    );
+  }
+}
